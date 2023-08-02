fb71b4b5a8bca9abdc3137b2a16c64e8e3f1a15f
IndianBartonka <bartonka2017@outlook.com>
2023-08-02 19:54:26 +0200

    Update ServerProperties.java

--- src/main/java/me/indian/bds/files/ServerProperties.java
+++ src/main/java/me/indian/bds/files/ServerProperties.java
@@ -31,7 +31,7 @@ public class ServerProperties {
             final InputStream input = Files.newInputStream(Paths.get(this.config.getFilesPath() + "/server.properties"));
             this.properties.clear();
             this.properties.load(input);
-        } catch (IOException e) {
+        } catch (final IOException e) {
             this.logger.critical(ConsoleColors.RED + "Wystąpił krytyczny błąd podczas ładowania " + ConsoleColors.GREEN + "server.properties" + ConsoleColors.RESET);
             this.bdsAutoEnable.getServerProcess().shutdown(false);
             throw new RuntimeException(e);
