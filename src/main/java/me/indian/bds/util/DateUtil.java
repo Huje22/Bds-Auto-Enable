1b676bb2faf30a528f98e1abdf714897e6f226f9
IndianBartonka <bartonka2017@outlook.com>
2023-08-14 16:50:24 +0200

    More methods

--- src/main/java/me/indian/bds/util/DateUtil.java
+++ src/main/java/me/indian/bds/util/DateUtil.java
@@ -1,5 +1,6 @@
 package me.indian.bds.util;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
@@ -9,12 +10,21 @@ public class DateUtil {
     public static String getFixedDate() {
         final LocalDateTime now = LocalDateTime.now(ZoneId.of("Europe/Warsaw"));
         final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-        return (now.format(formatter) + " ").replace(":", "-");
+        return now.format(formatter).replace(":", "-");
     }
 
     public static String getDate() {
         final LocalDateTime now = LocalDateTime.now(ZoneId.of("Europe/Warsaw"));
         final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
         return now.format(formatter);
+    }
+
+    public static long localDateToLong(final LocalDate localDate) {
+        return localDate.toEpochDay();
+    }
+
+
+    public static LocalDate longToLocalDate(final long days) {
+        return LocalDate.ofEpochDay(days);
     }
 }
