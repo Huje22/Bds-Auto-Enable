05256d2955a02e6fe32170073833e6f5d3aee630
IndianBartonka <bartonka2017@outlook.com>
2023-08-14 18:36:01 +0200

    Some changes

--- src/main/java/me/indian/bds/manager/PlayerManager.java
+++ src/main/java/me/indian/bds/manager/PlayerManager.java
@@ -1,7 +1,5 @@
 package me.indian.bds.manager;
 
-import me.indian.bds.util.DateUtil;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -12,37 +10,37 @@ import java.util.regex.Pattern;
 public class PlayerManager {
 
     private final List<String> onlinePlayers, offlinePlayers;
-    private final Map<String, String> onlineWithTime, offlineWithTime;
+    private final Map<String, Long> savedPlayers;
 
     public PlayerManager() {
         this.onlinePlayers = new ArrayList<>();
         this.offlinePlayers = new ArrayList<>();
-        this.onlineWithTime = new HashMap<>();
-        this.offlineWithTime = new HashMap<>();
+        this.savedPlayers = new HashMap<>();
     }
 
     public void updatePlayerList(final String logEntry) {
-        final String patternString = "(Player connected|Player disconnected): ([^,]+),";
+        final String patternString = "(Player connected|Player disconnected): ([^,]+), xuid: ([0-9]{16})";
         final Pattern pattern = Pattern.compile(patternString);
         final Matcher matcher = pattern.matcher(logEntry);
 
         if (matcher.find()) {
             final String action = matcher.group(1);
             final String playerName = matcher.group(2);
+            final String xuidString = matcher.group(3);
+            final Long xuid = Long.parseLong(xuidString);
 
             if ("Player connected".equals(action)) {
                 this.onlinePlayers.add(playerName);
-                this.onlineWithTime.put(playerName, DateUtil.getDate());
                 this.offlinePlayers.remove(playerName);
-                this.offlineWithTime.remove(playerName);
             } else if ("Player disconnected".equals(action)) {
                 this.onlinePlayers.remove(playerName);
-                this.onlineWithTime.remove(playerName);
                 this.offlinePlayers.add(playerName);
-                this.offlineWithTime.put(playerName, DateUtil.getDate());
             }
+
+            if (!this.savedPlayers.containsKey(playerName)) this.savedPlayers.put(playerName, xuid);
         }
     }
+
 
     public void clearPlayers() {
         this.onlinePlayers.clear();
@@ -56,12 +54,7 @@ public class PlayerManager {
         return this.offlinePlayers;
     }
 
-    public Map<String, String> getOfflineWithTime() {
-        return this.offlineWithTime;
+    public Map<String, Long> getSavedPlayers() {
+        return this.savedPlayers;
     }
-
-    public Map<String, String> getOnlineWithTime() {
-        return this.onlineWithTime;
-    }
-
 }
