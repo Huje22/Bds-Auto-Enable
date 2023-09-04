416021c2bb2de22db1ce1ec69e6ffc6f92fd3688
IndianBartonka <bartonka2017@outlook.com>
2023-09-05 00:12:43 +0200

    Better gson

--- src/main/java/me/indian/bds/util/GsonUtil.java
+++ src/main/java/me/indian/bds/util/GsonUtil.java
@@ -5,19 +5,10 @@ import com.google.gson.GsonBuilder;
 
 public final class GsonUtil {
 
-    private static final Gson gson = new Gson();
-    private static final Gson gsonBuilder = new GsonBuilder().create();
-    private static final Gson gsonPrettyBuilder = new GsonBuilder().setPrettyPrinting().create();
+    private static final Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
     public static Gson getGson() {
         return gson;
     }
 
-    public static Gson getGsonBuilder() {
-        return gsonBuilder;
-    }
-
-    public static Gson getGsonPrettyBuilder() {
-        return gsonPrettyBuilder;
-    }
 }
