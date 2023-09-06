e382bc0129d8d7bf82f48be2fca0d117ad1acdcd
IndianBartonka <68228599+IndianBartonka@users.noreply.github.com>
2023-09-06 21:42:25 +0200

    [Phone] Better Minecraft version info v2

--- src/main/java/me/indian/bds/bstats/OurMetrics.java
+++ src/main/java/me/indian/bds/bstats/OurMetrics.java
@@ -6,6 +6,5 @@ public class OurMetrics {
 
     public OurMetrics(final BDSAutoEnable bdsAutoEnable) {
         final Metrics metrics = new Metrics(bdsAutoEnable);
-        metrics.addCustomChart(new Metrics.SimplePie("bds_loaded_version", () -> bdsAutoEnable.getConfig().getVersion()));
     }
 }
