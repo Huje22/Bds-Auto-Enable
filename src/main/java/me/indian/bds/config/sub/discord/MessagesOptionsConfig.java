eab03b521fdc71e53a068985dca8a41c912a564b
IndianBartonka <bartonka2017@outlook.com>
2023-09-09 13:19:57 +0200

    Update MessagesOptionsConfig.java

--- src/main/java/me/indian/bds/config/sub/discord/MessagesOptionsConfig.java
+++ src/main/java/me/indian/bds/config/sub/discord/MessagesOptionsConfig.java
@@ -7,34 +7,34 @@ public class MessagesOptionsConfig extends OkaeriConfig {
 
     @Comment({""})
     @Comment({"Wysyłać wiadomość o zrobieniu backup"})
-    private boolean sendBackupMessage = false;
+    private boolean sendBackupMessage = true;
     @Comment({""})
     @Comment({"Wysyłać wiadomość o dołączeniu gracza"})
-    private boolean sendJoinMessage = false;
+    private boolean sendJoinMessage = true;
     @Comment({""})
     @Comment({"Wysyłać wiadomość o opuszczeniu gracza"})
-    private boolean sendLeaveMessage = false;
+    private boolean sendLeaveMessage = true;
     @Comment({""})
     @Comment({"Wysyłać wiadomość gracza"})
-    private boolean sendPlayerMessage = false;
+    private boolean sendPlayerMessage = true;
     @Comment({""})
     @Comment({"Wysyłać wiadomość o śmierci gracza"})
-    private boolean sendDeathMessage = false;
+    private boolean sendDeathMessage = true;
     @Comment({""})
     @Comment({"Wysyłać wiadomość o wyłączeniu servera"})
-    private boolean sendDisabledMessage = false;
+    private boolean sendDisabledMessage = true;
     @Comment({""})
     @Comment({"Wysyłać wiadomość o wyłączaniu servera"})
-    private boolean sendDisablingMessage = false;
+    private boolean sendDisablingMessage = true;
     @Comment({""})
     @Comment({"Wysyłać wiadomość o włączeniu procesu servera"})
-    private boolean sendProcessEnabledMessage = false;
+    private boolean sendProcessEnabledMessage = true;
     @Comment({""})
     @Comment({"Wysyłać wiadomość o włączeniu servera"})
-    private boolean sendEnabledMessage = false;
+    private boolean sendEnabledMessage = true;
     @Comment({""})
     @Comment({"Wysyłać wiadomość o zniszczeniu procesu"})
-    private boolean sendDestroyedMessage = false;
+    private boolean sendDestroyedMessage = true;
 
 
     public boolean isSendBackupMessage() {
