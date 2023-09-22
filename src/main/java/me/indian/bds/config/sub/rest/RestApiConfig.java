d33bb44a9ee2b00dfab2c782dfdb760676d1f22e
IndianBartonka <bartonka2017@outlook.com>
2023-09-23 00:38:01 +0200

    Added download backup rest api endpoint

--- src/main/java/me/indian/bds/config/sub/rest/RestApiConfig.java
+++ src/main/java/me/indian/bds/config/sub/rest/RestApiConfig.java
@@ -3,11 +3,17 @@ package me.indian.bds.config.sub.rest;
 import eu.okaeri.configs.OkaeriConfig;
 import eu.okaeri.configs.annotation.Comment;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class RestApiConfig extends OkaeriConfig {
 
-
     @Comment({""})
-    @Comment({"Dostępne endpointy to \"/api/stats/deaths\" , \"/api/stats/playtime\" , \"/api/stats/players\" "})
+    @Comment({"Dostępne endpointy to:"})
+    @Comment({"/api/stats/deaths - śmierci graczy   "})
+    @Comment({"/api/stats/playtime - czas gry w ms graczy "})
+    @Comment({"/api/stats/players - gracze online i offline"})
+    @Comment({"/api/{api-key}/backup/{filename} - pobierz któryś z dostępnych backup (wymagany klucz autoryzacji)"})
     @Comment({""})
     @Comment({"Czy włączyć strone z Rest API?"})
     private boolean enabled = true;
@@ -16,7 +22,10 @@ public class RestApiConfig extends OkaeriConfig {
     private int port = 8080;
     @Comment({""})
     @Comment({"Rate limit na dany endpoint"})
-    private int rateLimit = 100;
+    private int rateLimit = 20;
+    @Comment({""})
+    @Comment({"Klucze api które możesz rozdać użytkownikom rest api aby mogli: pobierać backupy , więcej wkrótce "})
+    private List<String> apiKeys = Arrays.asList("kOpsAjdfads", "KYFgvHVY");
 
     public boolean isEnabled() {
         return this.enabled;
@@ -28,5 +37,9 @@ public class RestApiConfig extends OkaeriConfig {
 
     public int getRateLimit() {
         return this.rateLimit;
+    }
+
+    public List<String> getApiKeys() {
+        return this.apiKeys;
     }
 }
--- src/main/java/me/indian/bds/rest/RestWebsite.java
+++ src/main/java/me/indian/bds/rest/RestWebsite.java
@@ -3,27 +3,42 @@ package me.indian.bds.rest;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import io.javalin.Javalin;
+import io.javalin.http.Context;
+import io.javalin.http.HttpStatus;
 import io.javalin.http.util.RateLimiter;
 import me.indian.bds.BDSAutoEnable;
 import me.indian.bds.config.sub.rest.RestApiConfig;
+import me.indian.bds.discord.DiscordIntegration;
 import me.indian.bds.logger.Logger;
 import me.indian.bds.manager.player.PlayerManager;
 import me.indian.bds.util.GsonUtil;
+import me.indian.bds.util.MessageUtil;
+import me.indian.bds.watchdog.module.BackupModule;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Path;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class RestWebsite {
 
     private final BDSAutoEnable bdsAutoEnable;
     private final Logger logger;
+    private final DiscordIntegration discordIntegration;
     private final RestApiConfig restApiConfig;
     private final PlayerManager playerManager;
+    private final BackupModule backupModule;
 
     public RestWebsite(final BDSAutoEnable bdsAutoEnable) {
         this.bdsAutoEnable = bdsAutoEnable;
         this.logger = this.bdsAutoEnable.getLogger();
+        this.discordIntegration = this.bdsAutoEnable.getDiscord();
         this.restApiConfig = this.bdsAutoEnable.getConfig().getRestApiConfig();
         this.playerManager = this.bdsAutoEnable.getPlayerManager();
+        this.backupModule = this.bdsAutoEnable.getWatchDog().getBackupModule();
     }
 
     public void init() {
@@ -37,6 +52,8 @@ public class RestWebsite {
 
             final Javalin app = Javalin.create().start(port);
             final RateLimiter limiter = new RateLimiter(TimeUnit.MINUTES);
+
+            app.after(ctx -> ctx.res().setCharacterEncoding("UTF-8"));
 
             app.get("/api/stats/playtime", ctx -> {
                 limiter.incrementCounter(ctx, rateLimit);
@@ -53,10 +70,64 @@ public class RestWebsite {
                 ctx.contentType("application/json").result(this.playersJson());
             });
 
+            app.get("/api/{api-key}/backup/{filename}", ctx -> {
+                limiter.incrementCounter(ctx, rateLimit);
+
+                if (!this.checkApiKey(ctx)) return;
+
+                final String filename = ctx.pathParam("filename");
+
+                for (final Path path : this.backupModule.getBackups()) {
+                    final String fileName = path.getFileName().toString().replaceAll(".zip", "");
+                    if (filename.equalsIgnoreCase(fileName)) {
+
+                        this.logger.info("&b" + ctx.ip() + "&r pobiera&3 " + filename);
+                        this.discordIntegration.writeConsole("`" + ctx.ip() + "` pobiera `" + filename + "`");
+
+                        final File file = new File(path.toString());
+                        ctx.res().setHeader("Content-Disposition", "attachment; filename=" + filename + ".zip");
+                        ctx.res().setHeader("Content-Type", "application/zip");
+
+                        try (final OutputStream os = ctx.res().getOutputStream(); final FileInputStream fis = new FileInputStream(file)) {
+                            final byte[] buffer = new byte[1024];
+                            int bytesRead;
+                            while ((bytesRead = fis.read(buffer)) != -1) {
+                                os.write(buffer, 0, bytesRead);
+                            }
+                        } catch (final IOException exception) {
+                            this.logger.error("", exception);
+                            ctx.status(500).contentType("application/json")
+                                    .result(exception.toString());
+                        }
+                        return;
+                    }
+                }
+
+                final List<String> backupsNames = this.backupModule.getBackupsNames();
+                backupsNames.replaceAll(name -> name.replaceAll(".zip", ""));
+
+                final String currentUrl = ctx.req().getRequestURL().toString().replaceAll(filename, "");
+                ctx.status(404).contentType("application/json").result("Dostępne Backupy to: \n"
+                        + MessageUtil.listToSpacedString(backupsNames) + "\n \n" +
+                        "Użyj np: " + currentUrl + backupsNames.get(0)
+                );
+            });
+
             this.logger.info("Uruchomiono strone z rest api na porcie:&b " + port);
         } catch (final Exception exception) {
             this.logger.error("Nie udało się uruchomić storny z&b Rest API", exception);
         }
+    }
+
+    private boolean checkApiKey(final Context ctx) {
+        final String apiKey = ctx.pathParam("api-key");
+
+        if (!this.restApiConfig.getApiKeys().contains(apiKey)) {
+            ctx.status(HttpStatus.UNAUTHORIZED).contentType("application/json")
+                    .result("Ten klucz api nie istnieje");
+            return false;
+        }
+        return true;
     }
 
     private String playersJson() {
