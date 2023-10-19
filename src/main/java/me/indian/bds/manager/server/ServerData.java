a72c31541111495fa82dafe18b1c8eebbc9e4927
IndianBartonka <68228599+IndianBartonka@users.noreply.github.com>
2023-10-19 11:24:39 +0200

    [Phone] add server data class

---
+++ src/main/java/me/indian/bds/manager/server/ServerData.java
@@ -0,0 +1,10 @@
+package me.indian.bds.manager.server;
+
+public class ServerData{
+
+private final long totalUpTime;
+
+  public ServerData(final long totalUpTime)(
+   this.totalUpTime = totalUpTime;
+
+}
