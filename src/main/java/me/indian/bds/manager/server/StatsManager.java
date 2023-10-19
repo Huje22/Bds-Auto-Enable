85915e0b380690e3dc8afa57d0bc0492f0388733
IndianBartonka <68228599+IndianBartonka@users.noreply.github.com>
2023-10-19 11:59:43 +0200

    [Phone] add server stats

--- src/main/java/me/indian/bds/manager/server/StatsManager.java
+++ src/main/java/me/indian/bds/manager/server/StatsManager.java
@@ -22,29 +22,32 @@ public class StatsManager {
 
     private final Logger logger;
     private final Timer playerStatsManagerTimer;
-    private final File statsFolder, playTimeJson, deathsJson;
+    private final File statsFolder, playTimeJson, deathsJson, serverStatsJson;
     private final Map<String, Long> playTime, deaths;
     private final ServerManager serverManager;
-    private boolean timerWorking = false;
-
+    private final ServerStats serverStats;
+    
+    
     public StatsManager(final BDSAutoEnable bdsAutoEnable, final ServerManager serverManager) {
         this.logger = bdsAutoEnable.getLogger();
         this.playerStatsManagerTimer = new Timer("PlayerStatsMonitorTimer", true);
         this.statsFolder = new File(Defaults.getAppDir() + "stats");
         this.playTimeJson = new File(this.statsFolder.getPath() + File.separator + "playtime.json");
         this.deathsJson = new File(this.statsFolder.getPath() + File.separator + "deaths.json");
+        this.serverStatsJson = new File(this.statsFolder.getPath() + File.separator + "server.json");
         this.createFiles();
         this.playTime = this.loadPlayTime();
         this.deaths = this.loadDeaths();
         this.serverManager = serverManager;
+        this.serverStats = this.loadServerStats();
     }
 
-    public void startTasks() {
-        if (this.timerWorking) return;
+    private void startTasks() {
         final long second = MathUtil.secondToMillis(1);
         final TimerTask playTimeTask = new TimerTask() {
             @Override
             public void run() {
+                this.serverStats.addOnlineTime(second);
                 for (final String playerName : StatsManager.this.serverManager.getOnlinePlayers()) {
                     StatsManager.this.playTime.put(playerName, StatsManager.this.getPlayTimeByName(playerName) + second);
                 }
@@ -60,7 +63,6 @@ public class StatsManager {
 
         this.playerStatsManagerTimer.scheduleAtFixedRate(saveDataTask, MathUtil.hoursToMillis(2), MathUtil.minutesToMillis(30));
         this.playerStatsManagerTimer.scheduleAtFixedRate(playTimeTask, 0, second);
-        this.timerWorking = true;
     }
 
     public Map<String, Long> getPlayTime() {
@@ -86,15 +88,19 @@ public class StatsManager {
     public void saveAllData() {
         this.savePlayTime();
         this.saveDeaths();
+        this.saveServerStats();
     }
+    
+    public ServerStats getServerStats(){
+        return this.serverStats;
+        }
 
     private void savePlayTime() {
         try (final FileWriter writer = new FileWriter(this.playTimeJson)) {
             writer.write(GsonUtil.getGson().toJson(this.playTime));
             this.logger.info("Pomyślnie zapisano&b czas gry&r graczy");
         } catch (final IOException exception) {
-            this.logger.critical("Nie udało się zapisać&b czasu gry&r graczy");
-            exception.printStackTrace();
+            this.logger.critical("Nie udało się zapisać&b czasu gry&r graczy", exception);
         }
     }
 
@@ -103,8 +109,16 @@ public class StatsManager {
             writer.write(GsonUtil.getGson().toJson(this.deaths));
             this.logger.info("Pomyślnie zapisano&r liczbe&b śmierci&r graczy");
         } catch (final IOException exception) {
-            this.logger.critical("Nie udało się zapisać liczby&b śmierci&r graczy");
-            exception.printStackTrace();
+            this.logger.critical("Nie udało się zapisać liczby&b śmierci&r graczy", exception);
+        }
+    }
+    
+    private void saveServerStats() {
+        try (final FileWriter writer = new FileWriter(this.serverStatsJson)) {
+            writer.write(GsonUtil.getGson().toJson(this.serverStats));
+            this.logger.info("Pomyślnie zapisano&b statystyki servera");
+        } catch (final IOException exception) {
+            this.logger.critical("Nie udało się zapisać liczby&b śmierci&r graczy", exception);
         }
     }
 
@@ -115,8 +129,7 @@ public class StatsManager {
             final HashMap<String, Long> loadedMap = GsonUtil.getGson().fromJson(reader, type);
             return (loadedMap == null ? new HashMap<>() : loadedMap);
         } catch (final IOException exception) {
-            this.logger.critical("Nie udało się załadować&b czasu gry&r graczy");
-            exception.printStackTrace();
+            this.logger.critical("Nie udało się załadować&b czasu gry&r graczy", exception);
         }
         return new HashMap<>();
     }
@@ -128,10 +141,18 @@ public class StatsManager {
             final HashMap<String, Long> loadedMap = GsonUtil.getGson().fromJson(reader, type);
             return (loadedMap == null ? new HashMap<>() : loadedMap);
         } catch (final IOException exception) {
-            this.logger.critical("Nie udało się załadować liczby&b śmierci&r graczy");
-            exception.printStackTrace();
-        }
+            this.logger.critical("Nie udało się załadować liczby&b śmierci&r graczy", exception);
+         }
         return new HashMap<>();
+    }
+    
+    private ServerStats loadServerStats() {
+        try (final FileReader reader = new FileReader(this.serverStatsJson)) {
+            return GsonUtil.getGson().fromJson(reader, ServerStats.class);
+        } catch (final IOException exception) {
+            this.logger.critical("Nie udało się załadować&b statystyk servera", exception);
+         }
+        return new ServerStats(0);
     }
 
     private void createFiles() {
@@ -141,8 +162,7 @@ public class StatsManager {
                     this.logger.critical("Nie udało się utworzyć folderu statystyk!");
                 }
             } catch (final Exception exception) {
-                this.logger.critical("Nie udało się utworzyć folderu statystyk!");
-                exception.printStackTrace();
+                this.logger.critical("Nie udało się utworzyć folderu statystyk!", exception);
             }
         }
 
@@ -155,7 +175,7 @@ public class StatsManager {
                     writer.write("\nDane są zapisywane do pliku co 30 minut albo co restart servera.");
                 }
             } catch (final IOException exception) {
-                this.logger.debug("Nie udało się stworzyć pliku informacyjnego w katalogu&b stats&r" + exception);
+                this.logger.debug("Nie udało się stworzyć pliku informacyjnego w katalogu&b stats&r" , exception);
             }
         }
 
@@ -165,19 +185,28 @@ public class StatsManager {
                     this.logger.critical("Nie można utworzyć&b playtime.json");
                 }
             } catch (final IOException exception) {
-                this.logger.critical("Nie udało się utworzyć&b playtime.json");
-                exception.printStackTrace();
+                this.logger.critical("Nie udało się utworzyć&b playtime.json", exception);
             }
         }
 
         if (!this.deathsJson.exists()) {
             try {
                 if (!this.deathsJson.createNewFile()) {
-                    this.logger.critical("Nie udało się  utworzyć&b playtime.json");
+                    this.logger.critical("Nie udało się  utworzyć&b deaths.json");
                 }
             } catch (final IOException exception) {
-                this.logger.critical("Nie można utworzyć&b deaths.json");
-                exception.printStackTrace();
+                this.logger.critical("Nie można utworzyć&b deaths.json", exception);
+            }
+        }
+        
+        
+        if (!this.serverStatsJson.exists()) {
+            try {
+                if (!this.serverStatsJson.createNewFile()) {
+                    this.logger.critical("Nie udało się  utworzyć&b server.json");
+                }
+            } catch (final IOException exception) {
+                this.logger.critical("Nie można utworzyć&b server.json", exception);
             }
         }
     }
