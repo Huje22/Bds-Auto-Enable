561f9207040ac227b1f4bf78360dc6120a520b4d
IndianBartonka <68228599+IndianBartonka@users.noreply.github.com>
2023-11-21 17:01:46 +0100

    Create LinkigManager.java

---
+++ src/main/java/me/indian/bds/discord/jda/manager/LinkigManager.java
@@ -0,0 +1,18 @@
+package me.indian.bds.discord.jda.manager;
+
+
+public class LinkingManager{
+
+
+  private final HashMap<String , Long> linkedAccounts;
+  
+  public LinkingManager(){
+
+
+   this. linkedAccounts = new HashMap<>();
+  }
+
+
+
+  
+}
