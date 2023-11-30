c2890f07fa661764796b8e43200dd0d0daa38efa
IndianBartonka <68228599+IndianBartonka@users.noreply.github.com>
2023-11-30 01:30:50 +0100

    [Phone] Add Todo

--- src/main/java/me/indian/bds/command/CommandManager.java
+++ src/main/java/me/indian/bds/command/CommandManager.java
@@ -6,6 +6,7 @@ import me.indian.bds.command.defaults.DeathsCommand;
 import me.indian.bds.command.defaults.HelpCommand;
 import me.indian.bds.command.defaults.LinkCommand;
 import me.indian.bds.command.defaults.PlaytimeCommand;
+import me.indian.bds.server.ServerProcess;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,10 +14,12 @@ import java.util.List;
 public class CommandManager {
 
     private final BDSAutoEnable bdsAutoEnable;
+    private final ServerProcess serverProcess;
     private final List<Command> commandList;
 
     public CommandManager(final BDSAutoEnable bdsAutoEnable) {
         this.bdsAutoEnable = bdsAutoEnable;
+        this.serverProcess = this.bdsAutoEnable.getServerProcess();
         this.commandList = new ArrayList<>();
         this.registerCommand(new HelpCommand(this.bdsAutoEnable, this.commandList));
         this.registerCommand(new BackupCommand(this.bdsAutoEnable));
@@ -35,12 +38,16 @@ public class CommandManager {
     }
 
     public void runCommands(final String playerName, final String commandName, final String[] args, final boolean isOp) {
+        
         for (final Command command : this.commandList) {
             if (command.getName().equals(commandName)) {
-                command.onExecute(playerName, args, isOp);
+              if (!command.onExecute(playerName, args, isOp)){
+                  //TODO: Gdy zostanie zwrócone false wypisać poprawne użycie komendy 
+                 this.serverProcess.tellrawToPlayer(playerName, command.getUsage());
+                }
                 return;
             }
         }
-        this.bdsAutoEnable.getServerProcess().tellrawToPlayer(playerName, "&cNie znaleziono takiego polecenia");
+        this.serverProcess.tellrawToPlayer(playerName, "&cNie znaleziono takiego polecenia");
     }
 }
