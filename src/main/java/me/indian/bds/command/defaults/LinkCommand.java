7378081b3ef2c2117dd77e48646ac1d1b5d32e29
IndianBartonka <bartonka2017@outlook.com>
2023-12-09 17:04:00 +0100

    Update LinkCommand.java

--- src/main/java/me/indian/bds/command/defaults/LinkCommand.java
+++ src/main/java/me/indian/bds/command/defaults/LinkCommand.java
@@ -12,13 +12,16 @@ public class LinkCommand extends Command {
     private final BDSAutoEnable bdsAutoEnable;
 
     public LinkCommand(final BDSAutoEnable bdsAutoEnable) {
-        super("link", "Połącz konta Discord i Minecraft", "&cPolecenie jest tylko dla graczy");
+        super("link", "Połącz konta Discord i Minecraft");
         this.bdsAutoEnable = bdsAutoEnable;
     }
 
     @Override
     public boolean onExecute(final String[] args, final boolean isOp) {
-        if (this.commandSender == CommandSender.CONSOLE) return false;
+        if (this.commandSender == CommandSender.CONSOLE){
+            this.sendMessage("&cPolecenie jest tylko dla graczy");
+            return true;
+        }
         if (this.bdsAutoEnable.getDiscord() instanceof final DiscordJda jda) {
             final LinkingManager linkingManager = jda.getLinkingManager();
             if (linkingManager == null) {
