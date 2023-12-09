88b79a9aa6a49278f9734c8a4a216b282f77425f
IndianBartonka <68228599+IndianBartonka@users.noreply.github.com>
2023-12-10 00:37:40 +0100

    Create Field.java

---
+++ src/main/java/me/indian/bds/discord/Field.java
@@ -0,0 +1,12 @@
+public class Field {
+    private String name;
+    private String value;
+    private boolean inline;
+
+    public Field(String name, String value, boolean inline) {
+        this.name = name;
+        this.value = value;
+        this.inline = inline;
+    }
+//TODO: Add getters
+}
