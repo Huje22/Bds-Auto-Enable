399e06714ae9dfec7bcc6626977ee9423338efb4
IndianBartonka <68228599+IndianBartonka@users.noreply.github.com>
2023-12-14 17:01:58 +0100

    [Phone] Create CompressionAlgorithm.java

---
+++ src/main/java/me/indian/bds/server/properties/CompressionAlgorithm.java
@@ -0,0 +1,17 @@
+package me.indian.bds.server.properties;
+
+public enum CompressionAlgorithm {
+  
+  ZLIB("zlib"),
+  SNAPPY("snappy");
+
+  private final String algorithmName;
+
+  CompressionAlgorithm(final String algorithmName){
+   this.algorithmName = algorithmName;
+}
+
+public String getAlgorithmName(){
+  return this.algorithmName;
+}                     
+}
