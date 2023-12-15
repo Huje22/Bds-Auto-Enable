60714026bb75b32c83e119b6eb6abeca20ae782d
IndianBartonka <bartonka2017@outlook.com>
2023-12-15 11:26:10 +0100

    Update CommandsConfig.java

--- src/main/java/me/indian/bds/config/sub/CommandsConfig.java
+++ src/main/java/me/indian/bds/config/sub/CommandsConfig.java
@@ -2,6 +2,7 @@ package me.indian.bds.config.sub;
 
 import eu.okaeri.configs.OkaeriConfig;
 import eu.okaeri.configs.annotation.Comment;
+import eu.okaeri.configs.annotation.CustomKey;
 import eu.okaeri.configs.annotation.Header;
 
 @Header("################################################################")
@@ -11,8 +12,9 @@ import eu.okaeri.configs.annotation.Header;
 
 public class CommandsConfig extends OkaeriConfig {
 
-    @Comment({})
-    @Comment({"czy !setting ma być dla każdego"})
+    @Comment({""})
+    @Comment({"Czy !setting ma być dla każdego"})
+    @CustomKey("SettingsForAll")
     private boolean settingsForAll = true;
 
 
