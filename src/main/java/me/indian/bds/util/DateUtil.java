d0d90d5c8a5ce863a482efa32c06880ea297f466
IndianBartonka <68228599+IndianBartonka@users.noreply.github.com>
2023-12-20 08:10:15 +0100

    [Phone] add TODO

--- src/main/java/me/indian/bds/util/DateUtil.java
+++ src/main/java/me/indian/bds/util/DateUtil.java
@@ -9,6 +9,8 @@ public final class DateUtil {
 
     public static final ZoneId POLISH_ZONE = ZoneId.of("Europe/Warsaw");
 
+//TODO: Dodać prywatne konstruktory di utili aby zapobiec robienia ich instancji 
+    
     public static String getFixedDate() {
         final LocalDateTime now = LocalDateTime.now(POLISH_ZONE);
         final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
