35e01f9c98f749469205d0421b8186ce64b7b631
IndianBartonka <68228599+IndianBartonka@users.noreply.github.com>
2024-01-15 13:52:12 +0100

    Create UnSupportedSystemException.java

---
+++ src/main/java/me/indian/bds/exception/UnSupportedSystemException.java
@@ -0,0 +1,8 @@
+package me.indian.bds.exception;
+
+public class UnSupportedSystemException extends RuntimeException {
+
+    public UnSupportedSystemException(final String message) {
+        super(message);
+    }
+}
