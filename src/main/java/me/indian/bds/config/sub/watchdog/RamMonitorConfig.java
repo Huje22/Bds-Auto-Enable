8e3227a2b4f1b05bd79bd96ed8d1925cc5526e92
IndianBartonka <bartonka2017@outlook.com>
2024-01-15 22:08:33 +0100

    Added cleaning cache on low memory

--- src/main/java/me/indian/bds/config/sub/watchdog/RamMonitorConfig.java
+++ src/main/java/me/indian/bds/config/sub/watchdog/RamMonitorConfig.java
@@ -14,12 +14,16 @@ public class RamMonitorConfig extends OkaeriConfig {
     private boolean machine = true;
 
     @Comment({""})
+    @Comment({"Czy wyczyścić pamięć cache gdy maszyna ma mało wolego ramu?"})
+    private boolean cleanCache = true;
+
+    @Comment({""})
     @Comment({"Czas sprawdzania stanu ram aplikacji w sekundach"})
-    private int checkAppTime = 5;
+    private int checkAppTime = 25;
 
     @Comment({""})
     @Comment({"Czas sprawdzania stanu ram maszyny w sekundach"})
-    private int checkMachineTime = 10;
+    private int checkMachineTime = 60;
 
     @Comment({""})
     @Comment({"Aletry na kanale czatu discord"})
@@ -32,6 +36,10 @@ public class RamMonitorConfig extends OkaeriConfig {
 
     public boolean isMachine() {
         return this.machine;
+    }
+
+    public boolean isCleanCache() {
+        return this.cleanCache;
     }
 
     public int getCheckAppTime() {
