29ef3117bfcd97d936308aa6171450ae7abbee98
IndianBartonka <68228599+IndianBartonka@users.noreply.github.com>
2024-01-15 13:49:14 +0100

    Create SystemUtil.java

---
+++ src/main/java/me/indian/bds/util/system/SystemUtil.java
@@ -0,0 +1,25 @@
+package me.indian.bds.util.system;
+
+public class SystemUtil {
+
+    public static SystemOS getSystem() {
+        final String os = System.getProperty("os.name").toLowerCase();
+
+        if (os.contains("win")) {
+            return SystemOS.WINDOWS;
+        } else if (os.contains("nix") || os.contains("nux")) {
+            return SystemOS.LINUX;
+//        } else if (os.contains("mac")) {
+//            return SystemOS.MAC;
+        } else {
+            return SystemOS.UNSUPPORTED;
+        }
+    }
+
+    public static String getFullyOsName() {
+        return System.getProperty("os.name");
+    }
+
+
+  
+}
