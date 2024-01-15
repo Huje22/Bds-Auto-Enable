43f222294527a6938e68cbae2c878f02ede683d2
IndianBartonka <68228599+IndianBartonka@users.noreply.github.com>
2024-01-15 14:06:09 +0100

    Update SystemUtil.java

--- src/main/java/me/indian/bds/util/system/SystemUtil.java
+++ src/main/java/me/indian/bds/util/system/SystemUtil.java
@@ -31,5 +31,39 @@ switch(getSystem()){
 // echo 1 > %SystemRoot%\System32\\DriverStore\EnforcementCache\EnforcementClientAggregateCache.dat
     }
   }
-  
+
+      public static long getRamUsageByPid(final long pid)  throws IOException , UnSupportedSystemException {
+            switch (SystemOS.getSystem()) {
+              case WINDOWS -> getMemoryUsageWindows(pid);
+                case LINUX -> getMemoryUsageLinux(pid);
+                case UNSUPORTED -> throw new UnSupportedSystemException("Nie można pozyskać ilość ram z nie wspieranego systemu");
+                
+            }
+        } 
+      }
+
+      private static long getMemoryUsageWindows(final long pid) throws IOException {
+        final Process process = Runtime.getRuntime().exec("tasklist /NH /FI \"PID eq " + pid + "\"");
+        try (final BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                if (line.contains(".exe")) {
+                    final String[] tokens = line.split("\\s+");
+                    if (tokens.length > 4) {
+                        final String memoryStr = tokens[4].replaceAll("\\D", "");
+                        return Long.parseLong(memoryStr);
+                    }
+                }
+            }
+        }
+        return -1;
+    }
+
+    private static long getMemoryUsageLinux(final long pid) throws IOException {
+        final Process process = Runtime.getRuntime().exec("ps -p " + pid + " -o rss=");
+        try (final BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+            final String line = reader.readLine();
+            return line != null ? Long.parseLong(line) : -1;
+        }
+    }
 }
