6a1c33f928e3988c7d5373a1ebcc010315d497eb
IndianBartonka <bartonka2017@outlook.com>
2024-01-17 00:39:46 +0100

    Update StatsCommand.java

--- src/main/java/me/indian/bds/command/defaults/StatsCommand.java
+++ src/main/java/me/indian/bds/command/defaults/StatsCommand.java
@@ -1,6 +1,7 @@
 package me.indian.bds.command.defaults;
 
 import me.indian.bds.command.Command;
+import me.indian.bds.command.CommandSender;
 import me.indian.bds.util.StatusUtil;
 
 public class StatsCommand extends Command {
@@ -11,6 +12,11 @@ public class StatsCommand extends Command {
 
     @Override
     public boolean onExecute(final String[] args, final boolean isOp) {
+        if(this.commandSender == CommandSender.PLAYER){
+            this.sendMessage("&cPolecenie można wykonać tylko z poziomu konsoli albo na discord&b /stats");
+            return  true;
+        }
+
         for (final String stats : StatusUtil.getMainStats(false)) {
             this.sendMessage(stats);
         }
