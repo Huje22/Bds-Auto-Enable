03662d355cfabea4796af4cb1700ae8aff1d413c
IndianBartonka <bartonka2017@outlook.com>
2024-02-01 19:47:49 +0100

    Added option to edit first page of RestWebsite

--- src/main/java/me/indian/bds/rest/RestWebsite.java
+++ src/main/java/me/indian/bds/rest/RestWebsite.java
@@ -13,28 +13,43 @@ import me.indian.bds.rest.post.key.PlayerInfoPostRequest;
 import me.indian.bds.rest.post.key.discord.DiscordMessagePostRequest;
 import me.indian.bds.rest.request.StatsRequest;
 import me.indian.bds.rest.request.key.BackupRequest;
+import me.indian.bds.util.DefaultsVariables;
 import me.indian.bds.util.GsonUtil;
+import me.indian.bds.util.MathUtil;
+import me.indian.bds.util.MessageUtil;
 import org.jetbrains.annotations.Nullable;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.concurrent.TimeUnit;
 
 public class RestWebsite {
 
+    private final BDSAutoEnable bdsAutoEnable;
     private final Javalin app;
     private final RateLimiter limiter;
     private final Logger logger;
     private final RestApiConfig restApiConfig;
     private final List<Request> requests;
+    private final File htmlFile;
+    private String htmlFileContent;
 
     public RestWebsite(final BDSAutoEnable bdsAutoEnable) {
+        this.bdsAutoEnable = bdsAutoEnable;
         this.app = Javalin.create();
         this.limiter = new RateLimiter(TimeUnit.MINUTES);
         this.logger = bdsAutoEnable.getLogger();
         this.restApiConfig = bdsAutoEnable.getAppConfigManager().getRestApiConfig();
         this.requests = new ArrayList<>();
+        this.htmlFile = new File(DefaultsVariables.getAppDir() + File.separator + "config" + File.separator + "Website.html");
 
+        this.createHTMLFile();
+        this.refreshFileContent();
         this.requests.add(new StatsRequest(this, bdsAutoEnable));
         this.requests.add(new BackupRequest(this, bdsAutoEnable));
         this.requests.add(new CommandPostRequest(this, bdsAutoEnable));
@@ -52,16 +67,13 @@ public class RestWebsite {
             this.app.after(ctx -> ctx.res().setCharacterEncoding("UTF-8"));
 
             this.app.get("/", ctx -> {
-                final String info = """
-                        Dostępne endointy to:
-                                                
-                        /api/stats/deaths - śmierci graczy\s
-                        /api/stats/playtime - czas gry w ms graczy\s
-                        /api/stats/players - gracze online i offline\s
-                        /api/{api-key}/backup/{filename} - pobierz któryś z dostępnych backup (wymagany klucz autoryzacji)\s
-                        """;
+                if (!this.htmlFile.exists()) this.createHTMLFile();
 
-                ctx.contentType("text/json").result(info);
+                ctx.contentType(ContentType.TEXT_HTML)
+                        .result(this.htmlFileContent
+                                .replaceAll("<online>", String.valueOf(this.bdsAutoEnable.getServerManager().getOnlinePlayers().size()))
+                                .replaceAll("<max>", String.valueOf(this.bdsAutoEnable.getServerProperties().getMaxPlayers()))
+                        );
             });
 
             for (final Request request : this.requests) {
@@ -102,6 +114,70 @@ public class RestWebsite {
 
         this.logger.debug("&b" + ip + "&r wysła niepoprawny json&1 " + requestBody.replaceAll("\n", ""), exception);
         ctx.status(HttpStatus.BAD_REQUEST).result("Niepoprawny Json! " + requestBody.replaceAll("\n", ""));
+    }
+
+    private void createHTMLFile() {
+        try {
+            if (!this.htmlFile.exists()) {
+                if (!this.htmlFile.createNewFile()) {
+                    this.logger.error("Nie można utworzyć&b Website.html");
+                }
+            }
+
+            if (Files.size(this.htmlFile.toPath()) == 0) {
+                try (final FileWriter writer = new FileWriter(this.htmlFile)) {
+                    writer.write("""                         
+                             <!DOCTYPE html>
+                            <html lang="pl">
+                            <meta name="theme-color" content="#38761d">
+                            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                            <meta charset="utf-8">
+                             <body>
+                                                       
+                            <a target="_blank" class="guzik" href="https://github.com/Huje22/Bds-Auto-Enable/blob/main/RestAPI.MD">Dostępne EndPointy</a><br>
+                            <a target="_blank" class="guzik" href="minecraft://?addExternalServer=Huje22|127.0.0.1:19132">Kliknij aby dołączyć na server!</a><br>
+                                    
+                                    Ta Strona odświeża się co 1min<br>
+                                    <br>
+                                    Tak możesz uzyskać osoby online na serwerze <br>
+                                    <online> / <max><br>
+                                                     
+                             <style>
+                             a { text-decoration: none; }
+                                                     
+                             .guzik:hover { box-shadow: 0px 4px 8px rgb(45 35 66 / 40%), 0px 7px 13px -3px rgb(45 35 66 / 30%), inset 0px -3px 0px #d6d6e7;
+                               transform: translateY(-2px);}
+                                                     
+                             .guzik:active { box-shadow: inset 0px 3px 7px #d6d6e7;
+                               transform: translateY(2px);}                 
+                             </style>
+                             </body>
+                             </html>
+                                                     
+                             """);
+                }
+            }
+        } catch (final Exception exception) {
+            this.logger.error("Nie udało się utworzyć&b Website.html", exception);
+        }
+    }
+
+    private void refreshFileContent() {
+        final TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                if (!RestWebsite.this.htmlFile.exists()) RestWebsite.this.createHTMLFile();
+
+                try {
+                    RestWebsite.this.htmlFileContent = MessageUtil.listToSpacedString(Files.readAllLines(RestWebsite.this.htmlFile.toPath()));
+                } catch (final Exception exception) {
+                    RestWebsite.this.htmlFileContent = "Strona odświeża<br><br> " + MessageUtil.getStackTraceAsString(exception);
+                }
+            }
+        };
+
+        final long minute = MathUtil.minutesTo(1, TimeUnit.MILLISECONDS);
+        new Timer("Refresh File Content", true).scheduleAtFixedRate(task, 0, minute);
     }
 
     public Javalin getApp() {
