c3a3b7a9acf344d890bc338492a2f553037631a9
IndianBartonka <68228599+IndianBartonka@users.noreply.github.com>
2024-02-04 20:10:22 +0100

    [Phone] (TODO) Create ExtensionMarketPlace.java

---
+++ src/main/java/me/indian/bds/extension/ExtensionMarketPlace.java
@@ -0,0 +1,10 @@
+package me.indian.bds.extension;
+
+public class ExtensionMarketPlace{
+
+
+//TODO: Dodać coś w w rodzaju markietu gdzie z repo github bedz json brane info o aktualnych rozserzeniah
+
+  
+
+}
