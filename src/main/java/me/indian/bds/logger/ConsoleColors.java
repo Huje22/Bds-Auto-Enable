0cc372059c75a998a552bea17e609dda8bf81821
IndianBartonka <bartonka2017@outlook.com>
2024-02-06 15:18:11 +0100

    improve `getMinecraftColorFromRGB` method

--- src/main/java/me/indian/bds/logger/ConsoleColors.java
+++ src/main/java/me/indian/bds/logger/ConsoleColors.java
@@ -8,6 +8,7 @@ public final class ConsoleColors {
 
     private static final Map<String, String> COLOR_MAP = new HashMap<>();
     private final static Map<String, ColorSet> COLORS = new HashMap<>();
+    private final static TreeMap<Double, String> CLOSEST = new TreeMap<>();
 
     private static final String RESET = "\u001B[0m";
     private static final String BOLD = "\033[1m";
@@ -102,22 +103,24 @@ public final class ConsoleColors {
         COLOR_MAP.put("^#e", BRIGHT_YELLOW_BACKGROUND);
         COLOR_MAP.put("^#f", BRIGHT_WHITE_BACKGROUND);
 
-        //TODO: Zniwelować częste występowanie białego 
         // WYMAGA TO WIELKICH USPRAWNIEŃ
         // Układane z pomocą https://minecraft.fandom.com/wiki/Formatting_codes
         COLORS.put(BLACK, new ColorSet(0, 0, 0));
+        COLORS.put(BLUE, new ColorSet(0, 0, 128));
         COLORS.put(DARK_BLUE, new ColorSet(0, 170, 0));
         COLORS.put(CYAN, new ColorSet(0, 170, 170));
         COLORS.put(DARK_RED, new ColorSet(170, 0, 0));
         COLORS.put(PURPLE, new ColorSet(170, 0, 170));
-        COLORS.put(GOLD, new ColorSet(255, 170, 0));
+        COLORS.put(LIGHT_PURPLE, new ColorSet(255, 85, 255));
+        COLORS.put(GOLD, new ColorSet(255, 165, 0));
         COLORS.put(BRIGHT_GRAY, new ColorSet(170, 170, 170));
         COLORS.put(DARK_GRAY, new ColorSet(85, 85, 85));
-        COLORS.put(BLUE, new ColorSet(85, 85, 255));
         COLORS.put(BRIGHT_CYAN, new ColorSet(85, 255, 255));
         COLORS.put(BRIGHT_RED, new ColorSet(255, 85, 85));
-        COLORS.put(LIGHT_PURPLE, new ColorSet(255, 85, 255));
+        COLORS.put(RED, new ColorSet(255, 0, 0));
         COLORS.put(YELLOW, new ColorSet(255, 255, 85));
+        COLORS.put(BRIGHT_GREEN, new ColorSet(0, 238, 43));
+        COLORS.put(GREEN, new ColorSet(0, 128, 0));
     }
 
     public static String getMinecraftColorFromANSI(final String ansi) {
@@ -125,6 +128,10 @@ public final class ConsoleColors {
             if (ansi.equals(entry.getValue())) return entry.getKey();
         }
         return "";
+    }
+
+    public static String getANSIFromRGB(final int r, final int g, final int b) {
+        return "\u001B[38;2;" + r + ";" + g + ";" + b + "m";
     }
 
     public static String convertMinecraftColors(final Object input) {
@@ -148,15 +155,19 @@ public final class ConsoleColors {
         return input.toString();
     }
 
+
     //Robione na podstawie https://github.com/PetteriM1/DiscordChat/blob/master/src/main/java/me/petterim1/discordchat/DiscordListener.java#L165
     //Nadal wymaga usprawnień
     public static String getMinecraftColorFromRGB(final int r, final int g, final int b) {
-        final TreeMap<Double, String> closest = new TreeMap<>();
+        if (r <= -1 && g <= -1 && b <= -1) return getMinecraftColorFromANSI(LIGHT_GRAY);
+
+        CLOSEST.clear();
         COLORS.forEach((color, set) -> {
             final double distance = Math.sqrt(Math.pow(r - set.red, 2) + Math.pow((g - set.green) * 1.5, 2) + Math.pow(b - set.blue, 2));
-            closest.put(distance, color);
+            CLOSEST.put(distance, color);
         });
-        return getMinecraftColorFromANSI(closest.firstEntry().getValue());
+
+        return getMinecraftColorFromANSI(CLOSEST.firstEntry().getValue());
     }
 
     private record ColorSet(int red, int green, int blue) {
