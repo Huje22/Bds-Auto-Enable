2898bafd3b2c470e85f77932f882a8bfbd184be2
IndianBartonka <bartonka2017@outlook.com>
2024-02-17 20:12:22 +0100

    Update ExtensionDescription.java

--- src/main/java/me/indian/bds/extension/ExtensionDescription.java
+++ src/main/java/me/indian/bds/extension/ExtensionDescription.java
@@ -2,12 +2,7 @@ package me.indian.bds.extension;
 
 import java.util.List;
 
-public record ExtensionDescription(String mainClass, String version, String name,String prefix,
-                                    String description, String author, List<String> authors, List<String> dependencies,
-                                   List<String> softDependencies
-
-) {
-
-    //TODO:Dodać "prefix" pod ExtensionLogger
-
+public record ExtensionDescription(String mainClass, String version, String name, String prefix,
+                                   String description, String author, List<String> authors, List<String> dependencies,
+                                   List<String> softDependencies) {
 }
