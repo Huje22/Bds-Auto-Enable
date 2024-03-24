42339ae897fe4d7597292e4987124eae5e569f5a
IndianBartonka <bartonka2017@outlook.com>
2024-03-24 15:52:39 +0100

    Update GeyserUtil.java

--- src/main/java/me/indian/bds/util/GeyserUtil.java
+++ src/main/java/me/indian/bds/util/GeyserUtil.java
@@ -1,6 +1,7 @@
 package me.indian.bds.util;
 
 import com.google.gson.JsonObject;
+import com.google.gson.annotations.SerializedName;
 import java.io.IOException;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -26,19 +27,19 @@ public class GeyserUtil {
 
     public static long getXuid(final String name) {
         try {
-            Request request = new Request.Builder()
+            final Request request = new Request.Builder()
                     .url("https://api.geysermc.org/v2/xbox/xuid/" + name)
                     .addHeader("Accept", "application/json")
                     .build();
 
-            try (Response response = client.newCall(request).execute()) {
+            try (final Response response = client.newCall(request).execute()) {
                 if (!response.isSuccessful()) {
                     System.out.println("Błąd przy pobieraniu danych. Kod odpowiedzi: " + response.code());
                     return -1;
                 }
 
-                String responseBody = response.body().string();
-                JsonObject jsonObject = GsonUtil.getGson().fromJson(responseBody, JsonObject.class);
+                final String responseBody = response.body().string();
+                final JsonObject jsonObject = GsonUtil.getGson().fromJson(responseBody, JsonObject.class);
 
                 if (jsonObject.has("xuid")) {
                     return jsonObject.get("xuid").getAsLong();
@@ -47,7 +48,7 @@ public class GeyserUtil {
                     return -1;
                 }
             }
-        } catch (IOException e) {
+        } catch (final IOException e) {
             e.printStackTrace();
             return -1;
         }
@@ -55,19 +56,19 @@ public class GeyserUtil {
 
     public static String getName(final long xuid) {
         try {
-            Request request = new Request.Builder()
+            final Request request = new Request.Builder()
                     .url("https://api.geysermc.org/v2/xbox/gamertag/" + xuid)
                     .addHeader("Accept", "application/json")
                     .build();
 
-            try (Response response = client.newCall(request).execute()) {
+            try (final Response response = client.newCall(request).execute()) {
                 if (!response.isSuccessful()) {
                     System.out.println("Błąd przy pobieraniu danych. Kod odpowiedzi: " + response.code());
                     return "";
                 }
 
-                String responseBody = response.body().string();
-                JsonObject jsonObject = GsonUtil.getGson().fromJson(responseBody, JsonObject.class);
+                final String responseBody = response.body().string();
+                final JsonObject jsonObject = GsonUtil.getGson().fromJson(responseBody, JsonObject.class);
 
                 if (jsonObject.has("gamertag")) {
                     return jsonObject.get("gamertag").getAsString();
@@ -76,18 +77,50 @@ public class GeyserUtil {
                     return "";
                 }
             }
-        } catch (IOException e) {
+        } catch (final IOException e) {
             e.printStackTrace();
             return "";
         }
     }
 
-    public static void main(final String[] args) {
+    public static void main(final String[] args) throws IOException {
         final String playerName = "JndjanBartonka";
         final long xuid = getXuid(playerName);
         System.out.println("XUID gracza " + playerName + ": " + xuid);
         final String nameFromXuid = getName(xuid);
         System.out.println(nameFromXuid);
+
+        System.out.println(getBedrockSkin(xuid));
     }
 
+
+    public static String getBedrockSkin(final long xuid) throws IOException {
+        final String geyserAPIResponse = sendHttpGETRequest(xuid);
+        if (geyserAPIResponse.equals("")) throw new RuntimeException();
+
+        return "https://textures.minecraft.net/texture/" + GsonUtil.getGson().fromJson(geyserAPIResponse, SkinResponse.class).getTextureId();
+    }
+
+    private static String sendHttpGETRequest(final long xuid) throws IOException {
+        final Request request = new Request.Builder()
+                .url("https://api.geysermc.org/v2/skin/" + xuid)
+                .build();
+
+        try (final Response response = client.newCall(request).execute()) {
+            if (response.isSuccessful()) {
+                return response.body().string();
+            } else {
+                return "";
+            }
+        }
+    }
+
+    static class SkinResponse {
+        @SerializedName("texture_id")
+        private String textureId;
+
+        public String getTextureId() {
+            return this.textureId;
+        }
+    }
 }
