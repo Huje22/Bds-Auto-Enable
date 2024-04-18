2483aa03a71e397ce98675c1d97c0c1366d8eebd
IndianBartonka <bartonka2017@outlook.com>
2024-04-18 21:08:45 +0200

    Added new Event system

--- src/main/java/me/indian/bds/event/EventManager.java
+++ src/main/java/me/indian/bds/event/EventManager.java
@@ -1,6 +1,8 @@
 package me.indian.bds.event;
 
+import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -10,34 +12,10 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicReference;
 import me.indian.bds.BDSAutoEnable;
-import me.indian.bds.event.player.PlayerBlockBreakEvent;
-import me.indian.bds.event.player.PlayerBlockPlaceEvent;
 import me.indian.bds.event.player.PlayerChatEvent;
-import me.indian.bds.event.player.PlayerCommandEvent;
-import me.indian.bds.event.player.PlayerDeathEvent;
-import me.indian.bds.event.player.PlayerDimensionChangeEvent;
-import me.indian.bds.event.player.PlayerInteractContainerEvent;
-import me.indian.bds.event.player.PlayerInteractEntityWithContainerEvent;
-import me.indian.bds.event.player.PlayerJoinEvent;
-import me.indian.bds.event.player.PlayerMuteEvent;
-import me.indian.bds.event.player.PlayerQuitEvent;
-import me.indian.bds.event.player.PlayerSpawnEvent;
-import me.indian.bds.event.player.PlayerUnMuteEvent;
 import me.indian.bds.event.player.response.PlayerChatResponse;
-import me.indian.bds.event.server.ExtensionDisableEvent;
-import me.indian.bds.event.server.ExtensionEnableEvent;
-import me.indian.bds.event.server.ServerAlertEvent;
-import me.indian.bds.event.server.ServerClosedEvent;
 import me.indian.bds.event.server.ServerConsoleCommandEvent;
-import me.indian.bds.event.server.ServerRestartEvent;
-import me.indian.bds.event.server.ServerStartEvent;
-import me.indian.bds.event.server.ServerUncaughtExceptionEvent;
-import me.indian.bds.event.server.ServerUpdatedEvent;
-import me.indian.bds.event.server.ServerUpdatingEvent;
-import me.indian.bds.event.server.TPSChangeEvent;
 import me.indian.bds.event.server.response.ServerConsoleCommandResponse;
-import me.indian.bds.event.watchdog.BackupDoneEvent;
-import me.indian.bds.event.watchdog.BackupFailEvent;
 import me.indian.bds.extension.Extension;
 import me.indian.bds.logger.Logger;
 import me.indian.bds.util.ThreadUtil;
@@ -71,93 +49,38 @@ public class EventManager {
 
             }
         });
+
         listenerToRemove.forEach(this.listenerMap::remove);
     }
 
+    /**
+     * Kod użyty z <a href="https://github.com/EnderHC-PL/EnderHC-Platform/blob/master/platform-eventbus/src/main/java/pl/enderhc/platform/eventbus/EventBus.java">...</a>
+     * Za zgodą Neziwa
+     * @param event - Event do wywołania w listenerach
+     */
     public void callEvent(final Event event) {
         this.listenerService.execute(() -> {
             this.listeners = new HashMap<>(this.listenerMap);
+            for (final Map.Entry<Listener, Extension> entry : this.listeners.entrySet()) {
+                final Listener listener = entry.getKey();
 
-            final AtomicReference<Extension> extension = new AtomicReference<>();
-
-            try {
-                //Player
-                this.listeners.forEach((listener, ex) -> {
-                    extension.set(ex);
-                    if (event instanceof final PlayerJoinEvent playerJoinEvent) {
-                        listener.onPlayerJoin(playerJoinEvent);
-                    } else if (event instanceof final PlayerSpawnEvent playerSpawnEvent) {
-                        listener.onPlayerSpawn(playerSpawnEvent);
-                    } else if (event instanceof final PlayerQuitEvent playerQuitEvent) {
-                        listener.onPlayerQuit(playerQuitEvent);
-                    } else if (event instanceof final PlayerDeathEvent playerDeathEvent) {
-                        listener.onPlayerDeath(playerDeathEvent);
-                    } else if (event instanceof final PlayerMuteEvent playerMuteEvent) {
-                        listener.onPlayerMute(playerMuteEvent);
-                    } else if (event instanceof final PlayerUnMuteEvent playerUnMuteEvent) {
-                        listener.onPlayerUnMute(playerUnMuteEvent);
-                    } else if (event instanceof final PlayerDimensionChangeEvent playerDimensionChangeEvent) {
-                        listener.onPlayerDimensionChange(playerDimensionChangeEvent);
-                    } else if (event instanceof final PlayerBlockBreakEvent playerBlockBreakEvent) {
-                        listener.onPlayerBreakBlock(playerBlockBreakEvent);
-                    } else if (event instanceof final PlayerBlockPlaceEvent playerBlockPlaceEvent) {
-                        listener.onPlayerPlaceBlock(playerBlockPlaceEvent);
-                    } else if (event instanceof final PlayerCommandEvent playerCommandEvent) {
-                        listener.onPlayerCommandEvent(playerCommandEvent);
-                    } else if (event instanceof final PlayerInteractContainerEvent playerInteractContainerEvent) {
-                        listener.onPlayerInteractContainerEvent(playerInteractContainerEvent);
-                    } else if (event instanceof final PlayerInteractEntityWithContainerEvent playerInteractEntityWithContainerEvent) {
-                        listener.onPlayerInteractEntityWithContainerEvent(playerInteractEntityWithContainerEvent);
+                final Method[] subscribeMethods = Arrays.stream(listener.getClass().getMethods())
+                        .filter(method -> method.isAnnotationPresent(EventHandler.class))
+                        .filter(method -> method.getParameters().length == 1)
+                        .filter(method -> method.getParameterTypes()[0].isAssignableFrom(event.getClass()))
+                        .toArray(Method[]::new);
+                for (final Method method : subscribeMethods) {
+                    try {
+                        method.setAccessible(true);
+                        method.invoke(listener, event);
+                        this.logger.debug("Wywołano&6 " + event.getEventName() +"&r dla&d " + listener.getClass().getName());
+                    } catch (final Throwable throwable) {
+                        this.logger.error("&cWystąpił błąd podczas wywoływania eventu:&1 " + event.getEventName() + "&c w listenerze:&1 " + listener.getClass().getName(), throwable);
                     }
-
-                    //Server
-                    else if (event instanceof final ServerStartEvent serverStartEvent) {
-                        listener.onServerStart(serverStartEvent);
-                    } else if (event instanceof final ServerRestartEvent serverRestartEvent) {
-                        listener.onServerRestart(serverRestartEvent);
-                    } else if (event instanceof final ServerClosedEvent serverClosedEvent) {
-                        listener.onServerClose(serverClosedEvent);
-                    } else if (event instanceof final ServerAlertEvent serverAlertEvent) {
-                        listener.onServerAlert(serverAlertEvent);
-                    } else if (event instanceof final TPSChangeEvent tpsChangeEvent) {
-                        listener.onTpsChange(tpsChangeEvent);
-                    } else if (event instanceof final BackupDoneEvent backupDoneEvent) {
-                        listener.onBackupDone(backupDoneEvent);
-                    } else if (event instanceof final BackupFailEvent backupFailEvent) {
-                        listener.onBackupFail(backupFailEvent);
-                    } else if (event instanceof final ServerUpdatingEvent serverUpdatingEvent) {
-                        listener.onServerUpdating(serverUpdatingEvent);
-                    } else if (event instanceof final ServerUpdatedEvent serverUpdatedEvent) {
-                        listener.onServerUpdated(serverUpdatedEvent);
-                    } else if (event instanceof final ServerUncaughtExceptionEvent serverUncaughtExceptionEvent) {
-                        listener.onServerUncaughtException(serverUncaughtExceptionEvent);
-                    }
-
-                    //Extension
-                    else if (event instanceof final ExtensionEnableEvent extensionEnableEvent) {
-                        listener.onExtensionEnable(extensionEnableEvent);
-                    } else if (event instanceof final ExtensionDisableEvent extensionDisableEvent) {
-                        listener.onExtensionDisable(extensionDisableEvent);
-                    } else {
-                        this.logger.error("Wywołano nieznany event&6 " + event.getEventName());
-                        return;
-                    }
-                });
-
-                this.logger.debug("Wywołano&6 " + event.getEventName());
-            } catch (final Exception exception) {
-                this.logger.critical("&cNie można wykonać eventu&b " + event.getEventName() + "&c dla rozszerzenia&b " + extension.get().getName(), exception);
+                }
             }
         });
     }
-
-//    public EventResponse callEventWithResponse(final ResponsibleEvent event) throws ExecutionException, InterruptedException {
-//        return CompletableFuture.supplyAsync(() -> this.getEventResponse(event), this.listenerService)
-//                .exceptionally(ex -> {
-//                    ex.printStackTrace();
-//                    return null;
-//                }).get();
-//    }
 
     public EventResponse callEventWithResponse(final ResponsibleEvent event) {
         return CompletableFuture.supplyAsync(() -> this.getEventResponse(event), this.listenerService).join();
--- src/main/java/me/indian/bds/event/Listener.java
+++ src/main/java/me/indian/bds/event/Listener.java
@@ -1,90 +1,18 @@
 package me.indian.bds.event;
 
-import me.indian.bds.event.player.PlayerBlockBreakEvent;
-import me.indian.bds.event.player.PlayerBlockPlaceEvent;
 import me.indian.bds.event.player.PlayerChatEvent;
-import me.indian.bds.event.player.PlayerCommandEvent;
-import me.indian.bds.event.player.PlayerDeathEvent;
-import me.indian.bds.event.player.PlayerDimensionChangeEvent;
-import me.indian.bds.event.player.PlayerInteractContainerEvent;
-import me.indian.bds.event.player.PlayerInteractEntityWithContainerEvent;
-import me.indian.bds.event.player.PlayerJoinEvent;
-import me.indian.bds.event.player.PlayerMuteEvent;
-import me.indian.bds.event.player.PlayerQuitEvent;
-import me.indian.bds.event.player.PlayerSpawnEvent;
-import me.indian.bds.event.player.PlayerUnMuteEvent;
 import me.indian.bds.event.player.response.PlayerChatResponse;
-import me.indian.bds.event.server.ExtensionDisableEvent;
-import me.indian.bds.event.server.ExtensionEnableEvent;
-import me.indian.bds.event.server.ServerAlertEvent;
-import me.indian.bds.event.server.ServerClosedEvent;
 import me.indian.bds.event.server.ServerConsoleCommandEvent;
-import me.indian.bds.event.server.ServerRestartEvent;
-import me.indian.bds.event.server.ServerStartEvent;
-import me.indian.bds.event.server.ServerUncaughtExceptionEvent;
-import me.indian.bds.event.server.ServerUpdatedEvent;
-import me.indian.bds.event.server.ServerUpdatingEvent;
-import me.indian.bds.event.server.TPSChangeEvent;
 import me.indian.bds.event.server.response.ServerConsoleCommandResponse;
-import me.indian.bds.event.watchdog.BackupDoneEvent;
-import me.indian.bds.event.watchdog.BackupFailEvent;
 import org.jetbrains.annotations.Nullable;
 
 public abstract class Listener {
 
     //Player
-    public void onPlayerJoin(final PlayerJoinEvent event) {
-
-    }
-
-    public void onPlayerSpawn(final PlayerSpawnEvent event) {
-
-    }
-
-    public void onPlayerQuit(final PlayerQuitEvent event) {
-
-    }
-
     @Nullable
     public PlayerChatResponse onPlayerChat(final PlayerChatEvent event) {
 
         return null;
-    }
-
-    public void onPlayerMute(final PlayerMuteEvent event) {
-
-    }
-
-    public void onPlayerUnMute(final PlayerUnMuteEvent event) {
-
-    }
-
-    public void onPlayerDeath(final PlayerDeathEvent event) {
-
-    }
-
-    public void onPlayerDimensionChange(final PlayerDimensionChangeEvent event) {
-
-    }
-
-    public void onPlayerBreakBlock(final PlayerBlockBreakEvent event) {
-
-    }
-
-    public void onPlayerPlaceBlock(final PlayerBlockPlaceEvent event) {
-
-    }
-
-    public void onPlayerCommandEvent(final PlayerCommandEvent event) {
-
-    }
-
-    public void onPlayerInteractContainerEvent(final PlayerInteractContainerEvent event) {
-
-    }
-
-    public void onPlayerInteractEntityWithContainerEvent(final PlayerInteractEntityWithContainerEvent event) {
-
     }
 
     //Server
@@ -92,51 +20,5 @@ public abstract class Listener {
     public ServerConsoleCommandResponse onServerConsoleCommand(final ServerConsoleCommandEvent event) {
 
         return null;
-    }
-
-    public void onServerStart(final ServerStartEvent event) {
-
-    }
-
-    public void onServerRestart(final ServerRestartEvent event) {
-
-    }
-
-    public void onServerClose(final ServerClosedEvent event) {
-
-    }
-
-    public void onServerAlert(final ServerAlertEvent event){
-
-    }
-
-    public void onServerUpdating(final ServerUpdatingEvent event) {
-
-    }
-
-    public void onServerUpdated(final ServerUpdatedEvent event) {
-
-    }
-
-    public void onTpsChange(final TPSChangeEvent event) {
-    }
-
-    public void onServerUncaughtException(final ServerUncaughtExceptionEvent event) {
-    }
-
-    public void onBackupDone(final BackupDoneEvent event) {
-
-    }
-
-    public void onBackupFail(final BackupFailEvent event) {
-
-    }
-
-    public void onExtensionEnable(final ExtensionEnableEvent event){
-
-    }
-
-    public void onExtensionDisable(final ExtensionDisableEvent event){
-
     }
 }
