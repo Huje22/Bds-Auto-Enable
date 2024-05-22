5d158d2cd4d852ada2731484fc7c8beb144d78db
IndianBartonka <bartonka2017@outlook.com>
2024-05-22 19:56:53 +0200

    Fix gson

--- src/main/java/me/indian/bds/util/GsonUtil.java
+++ src/main/java/me/indian/bds/util/GsonUtil.java
@@ -3,7 +3,6 @@ package me.indian.bds.util;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.Strictness;
-import java.lang.reflect.Modifier;
 
 public final class GsonUtil {
 
@@ -11,7 +10,6 @@ public final class GsonUtil {
             .setPrettyPrinting().serializeNulls()
             .disableHtmlEscaping()
             .setStrictness(Strictness.LENIENT)
-            .excludeFieldsWithModifiers(Modifier.PRIVATE)
             .create();
 
     private GsonUtil() {
